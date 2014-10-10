b53372bedcc849f14d88185b7ce0cd64904a535e
Steve Adams <steve@3sidedcube.com>
2014-10-10 12:10:33 +0100

    Add hashing method to the Api\Request\Files\Upload Class

--- library/classes/api/request/files/Upload.php
+++ library/classes/api/request/files/Upload.php
@@ -10,6 +10,11 @@
 		public $location;
 		public $error;
 
+		public function hash($algorithm = "md5")
+		{
+			return hash($algorithm, file_get_contents($this->location()));
+		}
+
 		public function exists()
 		{
 			return file_exists($this->location());
